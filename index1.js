fa6e75af0b230784e44fdcf3decbf8e1e3950ae3
gagan1146 <gagansharma99690@gmail.com>
2024-10-18 15:38:56 +0530

    javascript Promises 2

--- index1.js
+++ index1.js
@@ -1,4 +1,3 @@
-
 // console.log("Hello World");
 // let a=10;
 // if(a>10) console.log("Greater Then 10");
@@ -31,77 +30,85 @@
 // document.body.getElementsByClassName("Hf").innerHTML="Hii";
 //-------------------------------------------------------------------------------------------------------------------
 console.log(document);
-div=document.getElementsByClassName("parent");
-// div[0].innerHTML="<marquee>Hello World</marquee>";
-const h1=document.createElement("h1");
-h1.innerHTML="ABESEC";
-div[0].style="color:red";
+div = document.getElementsByClassName("parent");
+const h1 = document.createElement("h1");
+h1.innerHTML = "ABESEC";
+div[0].style.color = "red";  // Corrected style assignment
 div[0].append(h1);
 
-const h2=document.createElement('img');
-h2.src='car.jpeg';
-h2.setAttribute('height','200px');
+const h2 = document.createElement('img');
+h2.src = 'car.jpeg';
+h2.setAttribute('height', '200px');
 div[0].append(h2);
 console.log(h2);
 // -------------------------------------------------------------------------------------------------------------------
-flag=0;
+let flag = 0;  // Changed from var to let
 
-function hf2(){
-    if(flag==0){
-        h1.style="color:blue";
-        document.body.style="width: 150px;height: 80px;background-color: yellow;transform: rotate(10deg);";
-        flag=1;
+function hf2() {
+    if (flag == 0) {
+        h1.style.color = "blue";  // Corrected style assignment
+        document.body.style = "width: 150px;height: 80px;background-color: yellow;transform: rotate(10deg);";
+        flag = 1;
+    } else {
+        h1.style.color = "blue";  // Corrected style assignment
+        document.body.style = "width: 150px;height: 80px;background-color: green;transform: rotate(-10deg);";
+        h1.style.color = "white";  // Corrected style assignment
+        flag = 0;
     }
-    else{
-        h1.style="color:blue";
-        document.body.style="width: 150px;height: 80px;background-color: green;transform: rotate(-10deg);";
-        h1.style="color:white";
-        flag=0;
-    }
-        
 }
 
 //-------------------------------------------------------------------------------------------------------------------
 
-const promise1= new Promise(
-    (resolve,reject)=>{
-        let a=12;
-        if(a>10){
+const promise1 = new Promise(
+    (resolve, reject) => {
+        let a = 12;
+        if (a > 10) {
             resolve("Value of a is resolved");
-        }
-        else{
+        } else {
             reject("Value of a is reject");
         }
     }
 );
 
-promise1.then((msg)=>(console.log(msg)))
-.catch(error=>(console.log(error)))
-.finally(msg=>(console.log("Resources have been closed")));
-
+promise1.then((msg) => console.log(msg))
+    .catch(error => console.log(error))
+    .finally(() => console.log("Resources have been closed"));  // Corrected finally function
 
 //-------------------------------------------------------------------------------------------------------------------
 
-
-const promise2= new Promise(
-    (resolve,reject)=>{
-        let b=Math.random()*150;
-        if(b>=100 && b<=200){
+const promise2 = new Promise(
+    (resolve, reject) => {
+        let b = Math.random() * 150;
+        if (b >= 100 && b <= 200) {
             resolve("Value of b is resolved");
-        }
-        else{
+        } else {
             reject("Value of b is reject");
         }
     }
 );
 
-promise2.then((msg)=>(console.log(msg)))
-.catch(error=>(console.log(error)))
-.finally(msg=>(console.log("Resources have been closed")));
-
+promise2.then((msg) => console.log(msg))
+    .catch(error => console.log(error))
+    .finally(() => console.log("Resources have been closed"));  // Corrected finally function
 
 //-------------------------------------------------------------------------------------------------------------------
+fetch("https://dummyjson.com/products")  // Corrected fetch call
+    .then(response => response.json())  // Chained to handle response
+    .then(res => {
+        res.products.forEach(product => {  // Changed map to forEach
+            const h3 = document.createElement("h6");
+            h3.innerHTML = product.id + " " + product.title;  // Corrected access to product
+            div[0].style.color = "red";  // Corrected style assignment
+            div[0].append(h3);
+            console.log(product.id + " " + product.title);  // Corrected access to product
+        });
+    });
+
 //-------------------------------------------------------------------------------------------------------------------
+const h3 = document.createElement("h1");
+h3.innerHTML = "ABESEC";
+div[0].style.color = "red";  // Corrected style assignment
+div[0].append(h3);
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
 //-------------------------------------------------------------------------------------------------------------------
